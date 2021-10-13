67717d807c01eccd3c0d8cd02aa73bc2f6df491b
RiccardoBonini <38753018+RiccardoBonini@users.noreply.github.com>
2021-10-13 17:49:26 +0200

    Add files via upload

---
+++ parties.py
@@ -0,0 +1,133 @@
+from flakon import JsonBlueprint
+from flask import abort, jsonify, request
+
+from bedrock_a_party.classes.party import CannotPartyAloneError, Party, NotInvitedGuestError, ItemAlreadyInsertedByUser, NotExistingFoodError
+
+parties = JsonBlueprint('parties', __name__)
+
+_LOADED_PARTIES = {}  # dict of available parties
+_PARTY_NUMBER = 0  # index of the last created party
+
+
+# TODO: complete the decoration
+@parties.route("/parties", methods = ['POST', 'GET'])
+def all_parties():
+    result = None
+    if request.method == 'POST':
+        try:
+            result = create_party(request)
+        except CannotPartyAloneError:
+            abort(400)
+
+    elif request.method == 'GET':
+        result = get_all_parties()
+
+    return result
+
+
+# TODO: complete the decoration
+@parties.route("/parties/loaded", methods = ['GET'])
+def loaded_parties():
+    # TODO: returns the number of parties currently loaded in the system
+    global _PARTY_NUMBER
+    result = jsonify({'loaded_parties': _PARTY_NUMBER})
+    return result
+
+# TODO: complete the decoration
+@parties.route("/party/<id>", methods = ['GET', 'DELETE'])
+def single_party(id):
+    global _LOADED_PARTIES
+    result = ""
+
+    # TODO: check if the party is an existing one
+    exists_party(id)
+    if 'GET' == request.method:
+        # TODO: retrieve a party
+        result = jsonify(_LOADED_PARTIES[str(id)].serialize())
+    elif 'DELETE' == request.method:
+        # TODO: delete a party
+        del _LOADED_PARTIES[str(id)]
+        result = jsonify({"msg": "Party deleted! :("})
+    return result
+
+
+# TODO: complete the decoration
+@parties.route("/party/<id>/foodlist", methods = ['GET'])
+def get_foodlist(id):
+    global _LOADED_PARTIES
+    result = ""
+
+    # TODO: check if the party is an existing one
+    exists_party(id)
+    if 'GET' == request.method:
+        # TODO: retrieve food-list of the party
+        result = jsonify({"foodlist": _LOADED_PARTIES[str(id)].get_food_list().serialize()})
+    return result
+
+
+# TODO: complete the decoration
+@parties.route("/party/<id>/foodlist/<user>/<item>", methods = ['POST', 'DELETE'])
+def edit_foodlist(id, user, item):
+    global _LOADED_PARTIES
+
+    # TODO: check if the party is an existing one
+    exists_party(id)
+
+    # TODO: retrieve the party
+    result = ""
+
+    if 'POST' == request.method:
+        # TODO: add item to food-list handling NotInvitedGuestError (401) and ItemAlreadyInsertedByUser (400)
+        try:
+            _LOADED_PARTIES.get(str(id)).add_to_food_list(item, user)
+        except NotInvitedGuestError:
+            abort(401)
+        except ItemAlreadyInsertedByUser:
+            abort(400)
+        result = jsonify({"food": item, "user": user})
+    if 'DELETE' == request.method:
+        # TODO: delete item to food-list handling NotExistingFoodError (400)
+        try:
+            _LOADED_PARTIES.get(str(id)).remove_from_food_list(item, user)
+        except NotExistingFoodError:
+            abort(400)
+        result = jsonify({"msg": "Food deleted!"})
+    return result
+
+#
+# # These are utility functions. Use them, DON'T CHANGE THEM!!
+# #
+
+def create_party(req):
+    global _LOADED_PARTIES, _PARTY_NUMBER
+
+    # get data from request
+    json_data = req.get_json()
+
+    # list of guests
+    try:
+        guests = json_data['guests']
+    except:
+        raise CannotPartyAloneError("you cannot party alone!")
+
+    # add party to the loaded parties lists
+    _LOADED_PARTIES[str(_PARTY_NUMBER)] = Party(_PARTY_NUMBER, guests)
+    _PARTY_NUMBER += 1
+
+    return jsonify({'party_number': _PARTY_NUMBER - 1})
+
+
+def get_all_parties():
+    global _LOADED_PARTIES
+
+    return jsonify(loaded_parties=[party.serialize() for party in _LOADED_PARTIES.values()])
+
+
+def exists_party(_id):
+    global _PARTY_NUMBER
+    global _LOADED_PARTIES
+
+    if int(_id) > _PARTY_NUMBER:
+        abort(404)  # error 404: Not Found, i.e. wrong URL, resource does not exist
+    elif not(_id in _LOADED_PARTIES):
+        abort(410)  # error 410: Gone, i.e. it existed but it's not there anymore
